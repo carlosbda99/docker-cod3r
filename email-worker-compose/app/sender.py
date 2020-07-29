62de538bd9a4fe3e68b154452e2bf34cded96945
Carlos Henrique <carlosbda99@hotmail.com>
2020-07-29 12:35:25 -0300

    Finalização do curso

--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -1,15 +1,22 @@
 import psycopg2
 import redis
 import json
+import os
 from bottle import Bottle, request
 
 class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
-        self.fila =redis.StrictRedis(host='queue', port=6379, db=0)
-        DSN = 'dbname=email_sender user=postgres host=db password=123'
-        self.conn = psycopg2.connect(DSN)
+
+        redis_host = os.getenv('REDIS_HOST', 'queue')
+        self.fila =redis.StrictRedis(host=redis_host, port=6379, db=0)
+
+        db_host = os.getenv('DB_HOST','db')
+        db_user = os.getenv('DB_USER','postgres')
+        db_name = os.getenv('DB_NAME', 'sender')
+        dsn = f'dbname={db_name} user={db_user} host={db_host} password=123'
+        self.conn = psycopg2.connect(dsn)
 
     def register_message(self, assunto, mensagem):
         SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
@@ -17,7 +24,7 @@ class Sender(Bottle):
         cur.execute(SQL, (assunto, mensagem))
         self.conn.commit()
         cur.close()
-        self.conn.close()
+        
         msg = {'assunto': assunto, 'mensagem': mensagem}
         self.fila.rpush('sender', json.dumps(msg))
 
